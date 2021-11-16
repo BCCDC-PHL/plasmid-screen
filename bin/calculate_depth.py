8d0e7d5db8e523a6340405ee0ad6cacf4812c94d
Dan Fornika <dfornika@gmail.com>
2021-11-16 14:02:08 -0800

    Add depth calculation script

---
+++ bin/calculate_depth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+
+def main(args):
+
+    all_positions = 0
+    positions_above_threshold = 0
+    for line in sys.stdin:
+        [chrom, pos, depth] = line.strip('\t').split()
+        all_positions += 1
+        if int(depth) >= args.min_depth:
+            positions_above_threshold += 1
+
+    print(','.join([
+        str(all_positions),
+        str(positions_above_threshold),
+        str(round(float(positions_above_threshold) / float(all_positions) * 100, 3)),
+    ]))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--min-depth', default=10)
+    args = parser.parse_args()
+    main(args)
