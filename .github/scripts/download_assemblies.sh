2b2318abcc7d845b1129d995b2d7310dd82cfe60
Dan Fornika <dfornika@gmail.com>
2024-06-19 16:34:37 -0700

    update

--- .github/scripts/download_assemblies.sh
+++ .github/scripts/download_assemblies.sh
@@ -3,14 +3,14 @@
 mkdir -p .github/data/assemblies
 
 rm -f .github/data/assemblies/GCF_024700185.1.zip
-rm -f .github/data/assemblies/GCF_024700185.1.fa
+rm -f .github/data/assemblies/GCF024700185.1.fa
 rm -f .github/data/assemblies/README.md
 
 curl -o .github/data/assemblies/GCF_024700185.1.zip  "https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/accession/GCF_024700185.1/download?include_annotation_type=GENOME_FASTA&include_annotation_type=SEQUENCE_REPORT&hydrated=FULLY_HYDRATED"
 
 unzip .github/data/assemblies/GCF_024700185.1.zip -d .github/data/assemblies
 
-mv .github/data/assemblies/ncbi_dataset/data/GCF_024700185.1/GCF_024700185.1_ASM2470018v1_genomic.fna .github/data/assemblies/GCF_024700185.1.fa
+mv .github/data/assemblies/ncbi_dataset/data/GCF_024700185.1/GCF_024700185.1_ASM2470018v1_genomic.fna .github/data/assemblies/GCF024700185.1.fa
 
 rm -r .github/data/assemblies/ncbi_dataset
 rm -f .github/data/assemblies/README.md
--- .github/scripts/run_tests_locally.sh
+++ .github/scripts/run_tests_locally.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+rm -rf .github/data/assemblies/*
+rm -rf .github/data/fastq/*
+rm -rf .github/data/mob-suite-db
+rm -rf .github/data/samplesheet.csv
+rm -rf .github/data/test_output
+
 .github/scripts/download_assemblies.sh
 
 .github/scripts/simulate_reads.sh
