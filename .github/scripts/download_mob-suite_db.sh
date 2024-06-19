9e8fe9ad05976cdda8ad8983014ee4f121bc7587
Dan Fornika <dfornika@gmail.com>
2024-06-19 14:53:38 -0700

    prepare mob-suite db prior to use

--- .github/scripts/download_mob-suite_db.sh
+++ .github/scripts/download_mob-suite_db.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+source ${HOME}/.bashrc
+
+eval "$(conda shell.bash hook)"
+
 mkdir -p .github/data
 
 pushd .github/data
@@ -11,5 +15,11 @@ tar -xzf data.tar.gz
 rm data.tar.gz
 
 mv data mob-suite-db
+
+conda activate plasmid-screen-35d122a137231eda3b8a0039d42f24f6
+
+mash sketch -i mob-suite-db/ncbi_plasmid_full_seqs.fas
+
+makeblastdb -in mob-suite-db/ncbi_plasmid_full_seqs.fas -dbtype nucl
 
 popd
