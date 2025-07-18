6e8812b6d6ab4bf8023ea50ea840d0d64e13b42a
Dan Fornika <dfornika@gmail.com>
2025-07-18 09:28:41 -0700

    build both docker and apptainer containers (#40)

--- .github/scripts/build_container_images_wave.sh
+++ .github/scripts/build_container_images_wave.sh
@@ -5,7 +5,7 @@ mkdir -p wave_images
 
 for env_yaml in environments/*.yml; do
     image_name=$(head -n 1 $env_yaml | cut -d ' ' -f 2)
-    echo "building image ${image_name} from file ${env_yaml}..."
+    echo "building singularity/apptainer image ${image_name} from file ${env_yaml}..."
     wave \
 	--conda-file ${env_yaml} \
 	--singularity \
@@ -13,10 +13,22 @@ for env_yaml in environments/*.yml; do
 	--await \
 	--output json \
 	| python -m json.tool \
-	| tee wave_images/${image_name}.json
-    echo "done building image ${image_name}"
-    cp wave_images/${image_name}.json artifacts/
-
+	| tee wave_images/${image_name}-singularity.json
+    echo "done building singularity/apptainer image ${image_name}"
+    cp wave_images/${image_name}-singularity.json artifacts/
 done
 
 
+for env_yaml in environments/*.yml; do
+    image_name=$(head -n 1 $env_yaml | cut -d ' ' -f 2)
+    echo "building docker image ${image_name} from file ${env_yaml}..."
+    wave \
+	--conda-file ${env_yaml} \
+	--freeze \
+	--await \
+	--output json \
+	| python -m json.tool \
+	| tee wave_images/${image_name}-docker.json
+    echo "done building docker image ${image_name}"
+    cp wave_images/${image_name}-docker.json artifacts/
+done
